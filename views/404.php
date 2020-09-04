718ed4fbc1d1e96c5a151795838ca1d0cbe96017
Rogeret Kevin <kevin95660@outlook.fr>
2020-09-04 18:04:01 +0200

    page 404 frontend

--- views/404.php
+++ views/404.php
@@ -1,27 +1,25 @@
 <?php include('includes/header.php'); ?>
 
+<main class="main-content">
+		<div class="container">
+			<div class="breadcrumb">
+				<a href="index.php">Accueil</a>
+				<span>404</span>
+			</div>
+		</div>
 
-<div class="content-wrapper">
-    
-    
-    <section class="content">
-      <div class="error-page">
-        <h2 class="headline text-warning"> 404</h2>
-
-        <div class="error-content">
+    <div class="fullwidth-block error-front">
+			<div class="container">
+        <div class="content col-md-8 col-md-push-2 col-sm-12">
+          <h2 class="headline text-warning"> 404</h2>
           <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oups! Page non trouvée.</h3>
-
           <p>
             Nous n'avons pas trouvé la page que vous cherchez.
             En attendant, vous pouvez <a href="index.php">retourner a l'accueil</a>.
           </p>
         </div>
-        
       </div>
-      
-    </section>
-    
-  </div>
-  
+    </div>
+</main>
 
 <?php include('includes/footer.php'); ?>
